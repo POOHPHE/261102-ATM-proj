492176685be1edf6618373eddca93a013c1f5c9b
FALLENSLAYER007 <37149762+POOHPHE@users.noreply.github.com>
2020-03-14 21:30:35 +0700

    Add files via upload
    Update OTP page

--- Bank_261102_proj.cpp
+++ Bank_261102_proj.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int Gamespeed = 100;
+
 int cursor=0;
 int end=0;
 string page="main";
@@ -13,28 +13,71 @@ struct People{
 	string pin;
 	long double money;
 };
-vector<People> all_customers; 
+vector<People> all_customers;  
 set<string> find_id;
 
-void print_number(string s){
-	string num[10][5]={{"   ___  ","  / _ \\ "," | | | |"," | |_| |","  \\___/ "},{"  _ "," / |"," | |"," | |"," |_|"	},	{"  ____  "," |___ \\ ","   __) |","  / __/ "," |_____|"	},
-	{"  _____ "," |___ / ","   |_ \\ ","  ___) |"," |____/ "	},
-	{"  _  _   "," | || |  "," | || |_ "," |__   _|","    |_|  "	},
-	{"  ____  "," | ___| "," |___ \\ ","  ___) |"," |____/ "	},
-	{"   __   ","  / /_  "," | '_ \\ "," | (_) |","  \\___/ "	},
-	{"  _____ "," |___  |","    / / ","   / /  ","  /_/   "	},
-	{"   ___  ","  ( _ ) ","  / _ \\ "," | (_) |","  \\___/ "	},
-	{"   ___  ","  / _ \\ "," | (_) |","  \\__, |","    /_/ "	}};
 
-	for(int i=0;i<5;i++){
-		cout<<"                                                       ";
-		for(int j=0;j<s.size();j++){
-			int k=s[j]-48;
-			cout<<num[k][i];
-			
-		}
-		cout<<endl;
+
+
+string create_otp(){//create otp
+	string otp="";
+	string set[36]={"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+	int cur;
+	for(int i=0;i<6;i++){
+		cur=rand()%36;
+		otp=otp+set[cur];
 	}
+	return otp;
+}
+void print_text(string s,string space){
+	string text_char[36][5]={
+							{"   ___  ","  / _ \\ "," | |_| |"," | |_| |","  \\___/ "},//0
+							{"  _ "," / |"," | |"," | |"," |_|"	},	//1
+							{"  ____  "," |___ \\ ","   __) |","  / __/ "," |_____|"	},//2
+							{"  _____ "," |___ / ","   |_ \\ ","  ___) |"," |____/ "	},//3
+							{"  _  _   "," | || |  "," | || |_ "," |__   _|","    |_|  "	},//4
+							{"  ____  "," | ___| "," |___ \\ ","  ___) |"," |____/ "	},//5
+							{"   __   ","  / /_  "," | '_ \\ "," | (_) |","  \\___/ "	},//6
+							{"  _____ "," |___  |","    / / ","   / /  ","  /_/   "	},//7
+							{"   ___  ","  ( _ ) ","  / _ \\ "," | (_) |","  \\___/ "	},//8
+							{"   ___  ","  / _ \\ "," | (_) |","  \\__, |","    /_/ "	},//9
+							{"    _    " ,"   / \\   " ,"  / _ \\  " ," / ___ \\ ", "/_/   \\_\\"},//a
+ 							{ " ____  " ,"| __ ) " ,"|  _ \\ " ,"| |_) |", "|____/ "},//b
+ 							{ "  ____ ", " / ___|" ,"| |    " ,"| |___ " ," \\____|"},//c
+ 							{" ____  " ,"|  _ \\ " ,"| | | |" ,"| |_| |" ,"|____/ " },//d
+ 							{ " _____ " , "| ____|", "|  _|  ", "| |___ ", "|_____|"},//e
+ 							{ " _____ ", "|  ___|", "| |_   ", "|  _|  ", "|_|    "},//f
+ 							{ "  ____ ", " / ___|", "| |  _ ", "| |_| |", " \\____|" },//g
+ 							{" _   _ ", "| | | |", "| |_| |", "|  _  |", "|_| |_|"},//h
+ 							{" ___ ", "|_ _|", " | | ", " | | ", "|___|"},//i
+ 							{"     _ ", "    | |", " _  | |", "| |_| |", " \\___/ "},//j
+ 							{" _  __", "| |/ /", "| ' / ", "| . \\ ", "|_|\\_\\"},//k
+ 							{" _     ", "| |    ", "| |    ", "| |___ ", "|_____|"},//l
+ 							{" __  __ ", "|  \\/  |", "| |\\/| |", "| |  | |", "|_|  |_|"},//m
+ 							{" _   _ ", "| \\ | |", "|  \\| |", "| |\\  |", "|_| \\_|"},//n
+ 							{"  ___  ", " / _ \\ ", "| | | |", "| |_| |", " \\___/ "},//o
+ 							{" ____  ", "|  _ \\ ", "| |_) |", "|  __/ ", "|_|    "},//p
+ 							{"  ___  ", " / _ \\ ", "| | | |", "| |_| |", " \\__\\_\\"},//q
+ 							{" ____  ", "|  _ \\ ", "| |_) |", "|  _ < ", "|_| \\_\\"},//r
+ 							{" ____  ", "/ ___| ", "\\___ \\ ", " ___) |", "|____/ "},//s
+ 							{" _____ ", "|_   _|", "  | |  ", "  | |  ", "  |_|  "},//t
+ 							{" _   _ ", "| | | |", "| | | |", "| |_| |", " \\___/ "},//u
+ 							{"__     __", "\\ \\   / /", " \\ \\ / / ", "  \\ V /  ", "   \\_/   "},//v
+ 							{"__        __", "\\ \\      / /", " \\ \\ /\\ / / ", "  \\ V  V /  ", "   \\_/\\_/   "},//w
+ 							{"__  __", "\\ \\/ /", " \\  / ", " /  \\ ", "/_/\\_\\"},//x
+ 							{"__   __", "\\ \\ / /", " \\ V / ", "  | |  ", "  |_|  "},//y
+ 							{" _____", "|__  /", "  / / ", " / /_ ", "/____|"}//z
+ 							};
+ 		for(int i=0;i<5;i++){
+ 			cout<<space;
+ 			for(int j=0;j<s.size();j++){
+ 				int index=s[j]-55;
+ 				if(s[j]>=48&&s[j]<=57){
+ 					index=s[j]-48;
+				 }
+ 				cout<<text_char[index][i]<<" ";
+			 }cout<<endl;
+		 }
 }
 
 
@@ -80,6 +123,177 @@ void print_logo(){
 	cout<<endl;cout<<endl;
 }
 
+void wrong_pass(){
+	cout<<"\t\t\t\t\t\t          .  ..__ .__..  ..__   .__ .__. __. __..  ..__..__ .__ "<<endl;
+	cout<<"\t\t\t\t\t\t          |  |[__)|  ||\\ |[ __  [__)[__](__ (__ |  ||  |[__)|  \\"<<endl;
+	cout<<"\t\t\t\t\t\t          |/\\||  \\|__|| \\|[_./  |   |  |.__).__)|/\\||__||  \\|__/"<<endl;
+}
+
+void otp_page(){
+	string otp=create_otp();
+	string s="";
+	int wrong=0;
+	while(true){
+		
+		system("cls");
+		cout<<"\n\n\n\n\n";
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+		//print_text("OTP","\t\t\t\t\t\t\t\t      ");
+		cout<<"\t\t\t\t\t               ####### ####### ######      #####  ####### ######  ####### "<<endl;
+ 		cout<<"\t\t\t\t\t               #     #    #    #     #    #     # #     # #     # #       "<<endl;
+ 		cout<<"\t\t\t\t\t               #     #    #    #     #    #       #     # #     # #       "<<endl;
+ 		cout<<"\t\t\t\t\t               #     #    #    ######     #       #     # #     # #####   "<<endl;
+ 		cout<<"\t\t\t\t\t               #     #    #    #          #       #     # #     # #       "<<endl;
+ 		cout<<"\t\t\t\t\t               #     #    #    #          #     # #     # #     # #       "<<endl;
+ 		cout<<"\t\t\t\t\t               #######    #    #           #####  ####### ######  ####### "<<endl;
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),10);
+		cout<<"\n\n\n\n\n\n\n";
+		print_text(otp,"\t\t\t\t\t\t\t  ");
+		if(s.size()<6){
+			if(GetAsyncKeyState(0x41) != 0){
+			s=s+"A";
+		}
+		else if(GetAsyncKeyState(0x42) != 0){
+			s=s+"B";
+		}
+		else if(GetAsyncKeyState(0x43) != 0){
+			s=s+"C";
+		}
+		else if(GetAsyncKeyState(0x44) != 0){
+			s=s+"D";
+		}
+		else if(GetAsyncKeyState(0x45) != 0){
+			s=s+"E";
+		}
+		else if(GetAsyncKeyState(0x46) != 0){
+			s=s+"F";
+		}
+		else if(GetAsyncKeyState(0x47) != 0){
+			s=s+"G";
+		}
+		else if(GetAsyncKeyState(0x48) != 0){
+			s=s+"H";
+		}
+		else if(GetAsyncKeyState(0x49) != 0){
+			s=s+"I";
+		}
+		else if(GetAsyncKeyState(0x4A) != 0){
+			s=s+"J";
+		}
+		else if(GetAsyncKeyState(0x4B) != 0){
+			s=s+"K";
+		}
+		else if(GetAsyncKeyState(0x4C) != 0){
+			s=s+"L";
+		}
+		else if(GetAsyncKeyState(0x4D) != 0){
+			s=s+"M";
+		}
+		else if(GetAsyncKeyState(0x4E) != 0){
+			s=s+"N";
+		}
+		else if(GetAsyncKeyState(0x4F) != 0){
+			s=s+"O";
+		}
+		else if(GetAsyncKeyState(0x50) != 0){
+			s=s+"P";
+		}
+		else if(GetAsyncKeyState(0x51) != 0){
+			s=s+"Q";
+		}
+		else if(GetAsyncKeyState(0x52) != 0){
+			s=s+"R";
+		}
+		else if(GetAsyncKeyState(0x53) != 0){
+			s=s+"S";
+		}
+		else if(GetAsyncKeyState(0x54) != 0){
+			s=s+"T";
+		}
+		else if(GetAsyncKeyState(0x55) != 0){
+			s=s+"U";
+		}
+		else if(GetAsyncKeyState(0x56) != 0){
+			s=s+"V";
+		}else if(GetAsyncKeyState(0x57) != 0){
+			s=s+"W";
+		}else if(GetAsyncKeyState(0x58) != 0){
+			s=s+"X";
+		}else if(GetAsyncKeyState(0x59) != 0){
+			s=s+"Y";
+		}else if(GetAsyncKeyState(0x5A) != 0){
+			s=s+"Z";
+		}else if(GetAsyncKeyState(0x30) != 0||GetAsyncKeyState(VK_NUMPAD0) != 0){
+			s=s+"0";
+		}else
+		if(GetAsyncKeyState(0x31) != 0||GetAsyncKeyState(VK_NUMPAD1) != 0){
+			s=s+"1";
+		}else
+		if(GetAsyncKeyState(0x32) != 0||GetAsyncKeyState(VK_NUMPAD2) != 0){
+			s=s+"2";
+		}else
+		if(GetAsyncKeyState(0x33) != 0||GetAsyncKeyState(VK_NUMPAD3) != 0){
+			s=s+"3";
+		}else
+		if(GetAsyncKeyState(0x34) != 0||GetAsyncKeyState(VK_NUMPAD4) != 0){
+			s=s+"4";
+		}else
+		if(GetAsyncKeyState(0x35) != 0||GetAsyncKeyState(VK_NUMPAD5) != 0){
+			s=s+"5";
+		}else
+		if(GetAsyncKeyState(0x36) != 0||GetAsyncKeyState(VK_NUMPAD6) != 0){
+			s=s+"6";
+		}else
+		if(GetAsyncKeyState(0x37) != 0||GetAsyncKeyState(VK_NUMPAD7) != 0){
+			s=s+"7";
+		}else
+		if(GetAsyncKeyState(0x38) != 0||GetAsyncKeyState(VK_NUMPAD8) != 0){
+			s=s+"8";
+		}else
+		if(GetAsyncKeyState(0x39) != 0||GetAsyncKeyState(VK_NUMPAD9) != 0){
+			s=s+"9";
+		}
+		}
+		
+		if(GetAsyncKeyState(VK_BACK) != 0){
+			s=s.substr(0,s.size()-1);
+		}cout<<"\n\n\n\n";
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),14);
+		
+		print_text(s,"\t\t\t\t\t\t\t ");
+		if(wrong==1){
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+			wrong_pass();
+			wrong=0;
+		}else{
+			cout<<"\n\n\n";
+		}
+		
+		
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),13);
+		printf("\n\n\n\n\n\n\t.__ .__ .___ __. __.   __..__ .__. __ .___.__ .__..__   .___..__.  .__ .__. __ .  .\n");
+		printf("\t[__)[__)[__ (__ (__   (__ [__)[__]/  `[__ [__)[__][__)    |  |  |  [__)[__]/  `|_/ \n");
+		printf("\t|   |  \\[___.__).__)  .__)|   |  |\\__.[___[__)|  ||  \\    |  |__|  [__)|  |\\__.|  \\\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+		
+		
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),0);
+		system("pause");
+		if(GetAsyncKeyState(VK_SPACE) != 0){system("cls");
+		::page="login";
+			break;
+		}
+		if(GetAsyncKeyState(VK_RETURN) != 0){
+			if(s==otp){
+				::page="profile";
+			}else{
+				wrong=1;
+			}
+			
+		}
+	}
+	
+}
 
 void print_login(int w){
 	string login[7]={  	" ___      _______  _______  ___   __    _ ",                             
@@ -148,10 +362,6 @@ void print_login(int w){
 void print_regis(int w){
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),11);
 
-		/*string regis[4]={	" _____  _____  _____  _____  _____  _____  _____  _____ ",
-							"| __  ||   __||   __||     ||   __||_   _||   __|| __  |",
-							"|    -||   __||  |  ||-   -||__   |  | |  |   __||    -|",
-							"|__|__||_____||_____||_____||_____|  |_|  |_____||__|__|"};*/
 							cout<<endl;
 	for(int i=0;i<4;i++){
 		char a=219;cout<<"          ";
@@ -185,7 +395,7 @@ void print_regis(int w){
 				cout<<" ";
 			}cout<<"  ";
 		}
-		//cout<<regis[i]<<endl;
+
 		char e=92;
 		if(i==0){
 			printf("  ___   ___    ___   ___   ___   _____   ___   ___ \n");
@@ -204,10 +414,6 @@ void print_regis(int w){
 void print_exit(int w){
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),8);
 
-	/*string exit[4]={	" _____  __ __  _____  _____ ",                            
-						"|   __||  |  ||     ||_   _|",                           
-						"|   __||-   -||-   -|  | |  ",                            
-						"|_____||__|__||_____|  |_|  ", };*/
 	cout<<endl;
 	for(int i=0;i<4;i++){
 		cout<<"       ";
@@ -261,16 +467,7 @@ void print_exit(int w){
 	}
 }
 
-string create_otp(){//create otp
-	string otp="";
-	string set[36]={"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-	int cur;
-	for(int i=0;i<6;i++){
-		cur=rand()%36;
-		otp=otp+set[cur];
-	}
-	return otp;
-}
+
 
 void text_size(int s){//change text size func()
 	CONSOLE_FONT_INFOEX cfi;
@@ -289,6 +486,7 @@ void find_id_all(string no){
 		printf("\n\n\t\t\t\t\t.___.__..  ..  ..__ \n");
 		printf("\t\t\t\t\t [__ |  ||  ||\\ ||  \\\n");
 		printf("\t\t\t\t\t |   |__||__|| \\||__/\n");
+		::page="otp";
 	}else{SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
 		
 		printf("\n\n\t\t\t\t\t     __ .__..  ..  ..__..___.  .___.__..  ..  ..__   .__. __  __ .__..  ..  ..___.  .  .      \n");
@@ -346,14 +544,17 @@ string acc_no="";
 			acc_no=acc_no.substr(0,acc_no.size()-1);
 		}
 		cout<<endl;
-		print_number(acc_no);
+		print_text(acc_no,"\t\t\t\t\t\t  ");
 		if(GetAsyncKeyState(VK_RETURN) != 0){
 			find_id_all(acc_no);
+			if(page=="otp"){
+				break;
+			}
 		}else{
 			cout<<"\n\n";
 		}
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),13);
-		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t.__ .__ .___ __. __.   __..__ .__. __ .___.__ .__..__   .___..__.  .__ .__. __ .  .\n");
+		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t.__ .__ .___ __. __.   __..__ .__. __ .___.__ .__..__   .___..__.  .__ .__. __ .  .\n");
 		printf("\t[__)[__)[__ (__ (__   (__ [__)[__]/  `[__ [__)[__][__)    |  |  |  [__)[__]/  `|_/ \n");
 		printf("\t|   |  \\[___.__).__)  .__)|   |  |\\__.[___[__)|  ||  \\    |  |__|  [__)|  |\\__.|  \\\n");
 		
@@ -444,6 +645,8 @@ int main(){srand(time(0));
 			main_menu();
 		}else if(page=="login"){
 			login();
+		}else if(page=="otp"){
+			otp_page();
 		}
 		
 		
